f3bee9e5589f74bfc09ae12e58e86e71dbf958f9
alexst1910 <alexst1910@gmail.com>
2025-02-23 03:31:23 +0200

    started fetching from the back-end

---
+++ Js/main.js
@@ -0,0 +1,33 @@
+const apiUrl = "http://localhost:8080";
+
+addEventListener("DOMContentLoaded", (event) => {
+  fetch(apiUrl + "/getVenue")
+    .then((response) => {
+      return response.json();
+    })
+    .then((venues) => {
+      console.log("fetched: " + venues);
+      displayVenues(venues);
+    })
+    .catch((error) => {
+      console.error("error:", error);
+    });
+});
+const displayVenues = (venues) => {
+  const locationsContainer = document.getElementById("locations-container");
+
+  venues.forEach((venue, idx) => {
+    const coverUrl = `http://localhost:8080${venue.cover}`;
+    const element = `
+         <div class="location-card">
+        <a href="../BookingPage/booking.html"
+          ><img src=${coverUrl} alt="Music Pub"
+        /></a>
+        <h1 class="location-title" id="music-pub">${venue.name}</h1>
+        <a href="../BookingPage/booking.html"
+          ><button class="book-button" type="button">Book a seat</button></a
+        >
+      </div>`;
+    locationsContainer.insertAdjacentHTML("beforeend", element);
+  });
+};
